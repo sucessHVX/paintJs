7e83fc7da36534305dd86afdf9d8f83e7206b22e
Jaemin Ha <woals3527@naver.com>
2022-01-23 14:55:27 +0900

    change commit

--- js/paint.js
+++ js/paint.js
@@ -1,3 +1,78 @@
-const title = document.querySelector("h1");
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d"); //2d로 픽셀을 컨트롤하기위해 getContext쓴다
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-title.innerText = "100백100백";
+canvas.width = 700; //캔버스 width와 height를 줘야 실행됨
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c"; //그릴 선들이 모두 이 색을 가짐
+ctx.lineWidth = 2.5; //선 굵기
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y); //moveTo에서 lineTo까지 연결
+    ctx.stroke(); //그릴때 선을 그어줌
+  }
+}
+
+function onMouseUp(event) {
+  painting = false;
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "채우기";
+  } else {
+    filling = true;
+    mode.innerText = "그리기";
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+//if로 체크
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
